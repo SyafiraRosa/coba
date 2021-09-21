ce18eee22900163366262f89e41dd9bc4888178a
SyafiraRosa <firarosa966@gmail.com>
2021-09-21 20:17:33 +0700

    coba

---
+++ Assets/Prefabs/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource playSound;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        playSound.Play();
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
